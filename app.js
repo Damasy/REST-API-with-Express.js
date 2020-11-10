03a71ac2acef7dca61e5f4091516d9e75dc4921d
903601 <super.nova.egy@gmail.com>
2020-11-11 01:25:27 +0200

    integration tests

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-mongoose.connect("mongodb://localhost/bookAPI", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+if(process.env.ENV) {
+  console.log('This is Test');
+    mongoose.connect("mongodb://localhost/bookAPI_Test", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+} else {
+  console.log('This is Development');
+    mongoose.connect("mongodb://localhost/bookAPI", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+}
 const Book = require("./models/bookModel");
 const bookRouter = require('./routes/bookRouter')(Book);
 const port = process.env.port || 3000;
@@ -21,6 +29,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to my Nodemon API!");
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log("Running on port ", port);
 });
+
+module.exports = app;
